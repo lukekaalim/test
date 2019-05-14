87fb1886d81a7a877f52702a3ae99d1982be9180
Luke Kaalim <lukekaalim@gmail.com>
2019-05-15 00:38:43 +1000

    added esm support and color and global reporting

--- lib.js
+++ lib.js
@@ -1,19 +1,25 @@
 const path = require('path');
 const clc = require('cli-color');
 
+let globalTestPromises = [];
+const getGlobalTestPromises = () => globalTestPromises;
+
 const testAll = async (testFuncs, filename) => {
   const relativePath = path.relative(process.cwd(), filename);
-  const failures = [];
-  const successes = [];
-  await Promise.all(testFuncs.map(async testFunc => {
+  console.log('starting:', clc.blue(testFuncs.map(testFunc => testFunc.name).join(' ')));
+  const testPromises = testFuncs.map(async testFunc => {
     try {
       await testFunc();
-      successes.push(testFunc.name);
+      return { type: 'success', testName: testFunc.name };
     } catch (error) {
-      console.error(clc.red(relativePath + ' => ' + error.stack));
-      failures.push(error);
+      console.error(clc.yellow(`\n[${relativePath}] ${testFunc.name}()`) + clc.red(`\n${error.stack}`));
+      return { type: 'failure', testName: testFunc.name }
     }
-  }));
+  });
+  globalTestPromises = globalTestPromises.concat(testPromises);
+  const testResults = await Promise.all(testPromises);
+  const successes = testResults.filter(testResult => testResult.type === 'success');
+  const failures = testResults.filter(testResult => testResult.type === 'failure');
   console.log(clc.yellow(`${relativePath} => Success: ${successes.length}, Failure: ${failures.length}`));
 };
 
@@ -21,3 +27,4 @@ const test = (estFuncName = testFunc.name, testFunc) => testAll([testFunc], [tes
 
 exports.test = test;
 exports.testAll = testAll;
+exports.getGlobalTestPromises = getGlobalTestPromises;
--- run.js
+++ run.js
@@ -2,26 +2,39 @@
 
 const glob = require('glob').sync;
 const clc = require('cli-color');
+const esm = require('esm');
+const { getGlobalTestPromises } = require('./lib');
 
-const run = () => {
-  const testingGlob = [process.cwd(), '**/*.test.js'].join('/');
-  console.log(clc.yellow(`Looking for ${testingGlob}`));
+const esmRequire = esm(module);
+
+const run = (testGlob) => {
+  const localTestingGlob = [process.cwd(), testGlob].join('/');
+  console.log(clc.yellow(`Looking for ${localTestingGlob}`));
   
-  const testFiles = glob(testingGlob)
+  const testFiles = glob(localTestingGlob)
     .filter(testFile => !testFile.includes('node_modules'));
 
   console.log(clc.green(`Found ${testFiles.length} tests`));
-  testFiles.map(testFile => {
+  testFiles.forEach(testFile => {
     try {
-      require(testFile)
+      esmRequire(testFile);
     } catch (error) {
       console.error(clc.red(testFile + ' => ' + error.stack));
     }
   });
+  Promise.all(getGlobalTestPromises()).then((testResults) => {
+    const successes = testResults.filter(testResult => testResult.type === 'success');
+    const failures = testResults.filter(testResult => testResult.type === 'failure');
+    const failed = failures.length > 0;
+
+    console.log(clc.bold((failed ? clc.red : clc.green)(`\nAll lk-test Tests => Success: ${successes.length}, Failure: ${failures.length}`)));
+    process.exitCode = failed ? 1 : 0;
+  });
 };
 
 if (require.main === module) {
-  run();
+  const glob = process.argv[2] || '**/*.test.js';
+  run(glob);
 }
 
 exports.run = run;
